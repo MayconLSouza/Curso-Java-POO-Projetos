dc9f6c218904c4028a1734e96f00af8b5aa91624
MayconLSouza <mayconl.martins@outlook.com>
2025-12-05 22:47:08 -0300

    Checkmate logic

--- Section23_Chess-System/chess-system/src/application/Program.java
+++ Section23_Chess-System/chess-system/src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
         
-        while (true)
+        while (!chessMatch.getCheckMate())
         {
             try 
             {
@@ -54,5 +54,8 @@ public class Program {
                 sc.nextLine();
             }
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
