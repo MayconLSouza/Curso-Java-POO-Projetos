77fa0e327bddc21c944782e1813c4794745d8533
MayconLSouza <mayconl.martins@outlook.com>
2025-12-13 18:44:11 -0300

    Endpoint to return a user's posts

--- Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/resources/UserResource.java
+++ Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mayconlsouza.workshopmongo.domain.Post;
 import com.mayconlsouza.workshopmongo.domain.User;
 import com.mayconlsouza.workshopmongo.dto.UserDTO;
 import com.mayconlsouza.workshopmongo.services.UserService;
@@ -64,5 +65,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id)
+    {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
