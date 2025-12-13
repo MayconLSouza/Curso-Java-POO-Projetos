8f764259f60b004be047d1fb620f7af31d22211a
MayconLSouza <mayconl.martins@outlook.com>
2025-12-13 18:34:07 -0300

    Referencing user posts

--- Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/config/Instantiation.java
+++ Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/config/Instantiation.java
@@ -38,6 +38,9 @@ public class Instantiation implements CommandLineRunner{
         Post post1 = new Post(null, LocalDate.parse("21/03/2025", fmt), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, LocalDate.parse("23/03/2025", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
--- Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/domain/User.java
+++ Section24_Workshop-MongoDB/workshopmongo/src/main/java/com/mayconlsouza/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.mayconlsouza.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -12,6 +15,9 @@ public class User implements Serializable{
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
     
     public User() {}
 
@@ -52,6 +58,14 @@ public class User implements Serializable{
         this.email = email;
     }
     
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }    
+    
     @Override
     public int hashCode() 
     {
@@ -77,5 +91,5 @@ public class User implements Serializable{
         } else if (!id.equals(other.id))
             return false;
         return true;
-    }    
+    }
 }
