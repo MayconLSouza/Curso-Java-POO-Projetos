535c9b128dddea0aa74218001caac576b88aed3e
MayconLSouza <mayconl.martins@outlook.com>
2026-05-02 19:25:48 -0300

    DepartmentService

---
+++ Section25_JavaFX/workshop-javafx-jdbc/src/model/services/DepartmentService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+	
+	public List<Department> findAll() {
+		List<Department> list = new ArrayList<>();
+		list.add(new Department(1, "HR"));
+		list.add(new Department(2, "IT"));
+		list.add(new Department(3, "R&D"));
+		list.add(new Department(4, "Accounting"));
+		
+		return list;
+	}
+}
